e62de4c6ec6521f9963bfbfaca98c2c0d4d637bd
Timur İsmihan <timurismihan@hotmail.com>
2023-08-11 02:29:50 +0300

    base initiated

--- metropolexpress/metropolexpress/Controllers/HomeController.cs
+++ metropolexpress/metropolexpress/Controllers/HomeController.cs
@@ -6,13 +6,6 @@ namespace metropolexpress.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
@@ -22,11 +15,16 @@ namespace metropolexpress.Controllers
         {
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [Route("hakkimizda")]
+       public ActionResult About()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
+        [Route("iletisim")]
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        
     }
 }
